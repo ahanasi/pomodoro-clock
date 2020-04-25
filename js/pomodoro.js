1ae1188effe056fdcf2ea4d2a9798e582c99ef24
Ahana <ahanasi@seas.upenn.edu>
2020-04-25 11:31:37 -0700

    Combine session and break functions within a single setInterval

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,26 +1,46 @@
 const display = document.querySelector(".display");
 const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
-let session = 2;
-let countdown;
+let session = 5;
+let sessionTime;
+let breakSession = 2;
+let isBreak = false;
 
 play.addEventListener('click', function () {
-
-    countdown = setInterval(function () {
-        session -= 1;
-        console.log(session);
-        if(session == 0){
-            resetTimer();
-        }
-    }, 1000);
+    countdown();
 });
 
 pause.addEventListener('click', function () {
     console.log("Pause");
-    clearInterval(countdown);
+    clearInterval(sessionTime);
 });
 
-function resetTimer(){
-    clearInterval(countdown);
-    session = 25;
+function countdown(){
+    sessionTime = setInterval(function(){
+        session -= 1;
+        console.log(`session: ${session}`);
+        if (session == 0){
+            // clearInterval(sessionTime);
+            isBreak = !isBreak;
+            if(!isBreak){
+                session = 5;
+                console.log("Session Start");
+            } else {
+                session = 2;
+                console.log('Break Start');
+            }
+        }
+    },1000);
 }
+
+
+
+
+
+
+
+
+
+
+
+
