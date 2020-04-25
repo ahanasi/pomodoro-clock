992077a8c1078b44d77fd81233740c0663d3e1e0
Ahana <ahanasi@seas.upenn.edu>
2020-04-25 12:44:22 -0700

    Activate display and create functions to stop and reset timer

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,12 +1,23 @@
 const display = document.querySelector(".display");
 const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
-let session = 5;
+const stopBtn = document.querySelector(".stop");
+const resetBtn = document.querySelector(".reset");
+const session = document.querySelector("#session");
+const breakSession = document.querySelector("#break");
+const times = document.querySelectorAll("#time");
+let result = session.value;
 let sessionTime;
-let breakSession = 2;
 let isBreak = false;
 
+resetTimer();
+session.addEventListener('change', function(){
+    result = session.value;
+});
+
 play.addEventListener('click', function () {
+    session.disabled = true;
+    breakSession.disabled = true;
     countdown();
 });
 
@@ -15,22 +26,46 @@ pause.addEventListener('click', function () {
     clearInterval(sessionTime);
 });
 
+stopBtn.addEventListener('click', stopTimer);
+
+resetBtn.addEventListener('click', resetTimer);
+
+
 function countdown(){
     sessionTime = setInterval(function(){
-        session -= 1;
-        console.log(`session: ${session}`);
-        if (session == 0){
-            // clearInterval(sessionTime);
+
+        result -= 1;
+        
+        display.textContent =  (!isBreak) ? `Session: ${result}`
+                                          : `Break: ${result}`;
+
+        console.log(`session: ${result}`);
+        if (result == 0){
             isBreak = !isBreak;
             if(!isBreak){
-                session = 5;
+                result = session.value;
                 console.log("Session Start");
             } else {
-                session = 2;
+                result = breakSession.value;
                 console.log('Break Start');
             }
         }
     },1000);
+}
+
+function resetTimer(){
+    session.disabled = false;
+    breakSession.disabled = false;
+    session.value = 25;
+    breakSession.value = 5;
+    clearInterval(sessionTime);
+}
+
+function stopTimer(){
+    session.disabled = false;
+    breakSession.disabled = false;
+    clearInterval(sessionTime);
+    display.textContent = `Session: ${session.value}`;
 }
 
 
