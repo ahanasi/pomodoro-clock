fb2197444e35908f846295b276906950a5d8306f
Ahana <ahanasi@seas.upenn.edu>
2020-04-24 20:11:44 -0700

    Create play and pause functions
    If applied, this commit starts the countdown once the play button is clicked.
    The pause button pauses the countdown and the interval is reset once the countdown hits 0.

---
+++ js/pomodoro.js
@@ -0,0 +1,30 @@
+const display = document.querySelector(".display");
+const play = document.querySelector(".play");
+const pause = document.querySelector(".pause");
+let session = 2;
+let countdown;
+
+play.addEventListener('click', function () {
+
+    countdown = setInterval(function () {
+        session -= 1;
+        console.log(session);
+        if(session == 0){
+            clearInterval(countdown);
+            session = 25;
+        }
+    }, 1000);
+
+});
+
+pause.addEventListener('click', function () {
+    console.log("Pause");
+    clearInterval(countdown);
+});
+
+
+
+
+
+
+
